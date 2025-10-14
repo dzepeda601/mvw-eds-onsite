a682eee08527aa923cc3faa2b34b9c300fde4f22
dzepeda601 <zepeda@adobe.com>
2025-10-14 14:16:10 -0700

    updating js

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -44,8 +44,7 @@ export async function loadFragment(path) {
 }
 
 export default async function decorate(block) {
-  const link = block.querySelector('a');
-  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  const path = block.dataset.cfPath; // Use data-cf-path instead of link or text
   const fragment = await loadFragment(path);
   if (fragment) {
     const fragmentSection = fragment.querySelector(':scope .section');
